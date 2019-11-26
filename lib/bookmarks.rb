067658caa6cf7512cf630fa36ccfe40e2dcc8715
Rebecca Varley <rebecca.varley@gmail.com>
2019-11-26 08:50:31 +0000

    Commit of all the work done on day 1 with Marjan

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -1,9 +1,6 @@
 class Bookmarks
 
- def initalize
 
- end
- 
   def all
     ["https://sqlzoo.net/wiki/SELECT_basics", "https://www.google.com/", "https://www.facebook.com", "https://www.asos.com"]
   end
