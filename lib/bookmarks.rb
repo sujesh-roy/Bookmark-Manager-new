021b9c52c4a96cc4c55b69cb7926c94022f256f0
Rebecca Varley <rebecca.varley@gmail.com>
2019-11-26 16:36:34 +0000

    Failing tests for refactoring the add method to include the database

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -1,8 +1,30 @@
+require 'pg'
+
 class Bookmarks
 
 
   def all
-    ["https://sqlzoo.net/wiki/SELECT_basics", "https://www.google.com/", "https://www.facebook.com", "https://www.asos.com"]
+    begin
+
+    con = PG.connect :dbname => 'bookmarks', :user => 'Bookmark_manager_database'
+
+    rs = con.exec "SELECT * FROM bookmarks"
+
+    rs.each do |row|
+      puts "%s %s" % [ row['id'], row['url'] ]
+    end
+
+rescue PG::Error => e
+
+    puts e.message
+
+ensure
+
+    rs.clear if rs
+    con.close if con
+
+end
+    # ["https://sqlzoo.net/wiki/SELECT_basics", "https://www.google.com/", "https://www.facebook.com", "https://www.asos.com"]
   end
 
 end
